9d36d580ac01e140890edcbd48632d038369e1ca
hyosua <colleterhyosua@gmail.com>
2025-04-18 06:08:22 +0200

    modif cors link

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ import { errorHandler } from './errorHandler.js'
 const app = express()
 
 app.use(cors({
-  origin: 'https://peakture-git-main-drykissfffos-projects.vercel.app/',
+  origin: 'https://peakture-gpumivoj7-drykissfffos-projects.vercel.app',
   credentials: true,
 }))
 app.use(express.json())
