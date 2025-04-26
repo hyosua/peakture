79fc3a25f558e52119a76396475557f209808259
hyosua <colleterhyosua@gmail.com>
2025-04-26 09:06:11 +0200

    add animations for profile editing

--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -4,6 +4,7 @@ import { useToast } from "../../context/ToastContext.jsx"
 import { User, Camera, Upload, Pencil } from "lucide-react";
 import { useAuth } from '../../context/AuthContext.jsx';
 import uploadToCloudinary from "../../../utils/uploadToCloudinary.js";
+import { motion } from "framer-motion";
 
 const Profile = () => {
   const [user, setUser] = useState({username: '', avatar: null, coverImage: null});
@@ -11,7 +12,8 @@ const Profile = () => {
   const [coverPreview, setCoverPreview] = useState(null);
   const {showToast} = useToast();
   const [isEditing, setIsEditing] = useState(false);
-  const formRef = useRef(null)
+  const [coverUploading, setCoverUploading] = useState(false)
+  const [avatarUploading, setAvatarUploading] = useState(false)
 
   const {currentUser, fetchCurrentUser} = useAuth();
 
@@ -35,11 +37,20 @@ const Profile = () => {
   const handleAvatarChange = async (e) => {
     setIsEditing(true);
     const file = e.target.files[0];
+    if (!file) return;
+      // Limite d'un upload sur Cloudinary: 10MB
+      if (file.size > 10 * 1024 * 1024) {
+        alert('La taille de l\'image ne doit pas dépasser 10MB');
+        return;
+    }
     try{
+      setAvatarUploading(true)
       const avatarUrl = await uploadToCloudinary(file);
       setUser({...user, avatar: avatarUrl});
       setAvatarPreview(avatarUrl);
+      setAvatarUploading(false)
     } catch (error){
+      setAvatarUploading(false)
       console.error("Error uploading avatar to Cloudinary: ", error);
       throw error;
     }
@@ -48,11 +59,21 @@ const Profile = () => {
   const handleCoverChange = async (e) => {
     setIsEditing(true);
     const file = e.target.files[0];
+    if (!file) return;
+    // Limite d'un upload sur Cloudinary: 10MB
+    if (file.size > 10 * 1024 * 1024) {
+      alert('La taille de l\'image ne doit pas dépasser 10MB');
+      return;
+  }
     try{
+      setCoverUploading(true)
       const coverUrl = await uploadToCloudinary(file);
       setUser({...user, coverImage: coverUrl});
       setCoverPreview(coverUrl);
+      setCoverUploading(false)
+
     } catch (error){
+      setCoverUploading(false)
       console.error("Error uploading cover image to Cloudinary: ", error);
       throw error;
     }
@@ -92,44 +113,83 @@ const Profile = () => {
           </button>
         </div>
       {/* Cover Image */}
-      <div className="relative h-48 bg-gray-200">
-          <img
-            src={coverPreview}
-            alt="Cover"
-            className="w-full h-full object-cover"
-          />
-        <label className="absolute bottom-3 right-3 bg-white p-2 rounded-full shadow-md cursor-pointer hover:bg-primary" title="Changer l'image de couverture">
-          <Camera size={20} className="text-gray-600" />
-          <input
-            type="file"
-            onChange={handleCoverChange}
-            className="hidden"
-            accept="image/*"
-          />
-        </label>
+      <div className="relative h-48 bg-neutral">
+        {coverUploading ? (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <span className="loading loading-bars text-primary loading-md"></span>
+          </div>
+        ):(
+        
+          <motion.img
+          key={coverPreview} 
+          src={coverPreview}
+          alt="Cover"
+          className="w-full h-full object-cover"
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 200, damping: 20 }}
+        />
+        )}
+          
+        {/* Bouton Camera */}
+        <motion.label
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="absolute bottom-3 right-3 bg-white p-2 rounded-full shadow-md cursor-pointer hover:bg-primary"            
+            title="Changer l'image de couverture"
+          >
+            <Camera size={20} className="text-gray-600" />
+            <input 
+              type="file" 
+              onChange={handleCoverChange} 
+              className="hidden" 
+              accept="image/*"
+            />
+          </motion.label>
       </div>
       
       <div className="p-6">
         <div className="flex gap-6 h-screen">
-          {/* Avatar Section */}
-          <div className="relative flex-shrink-0">
-            <div className="w-32 h-32 rounded-full border-4 border-white shadow-lg -mt-16 overflow-hidden bg-gray-200">
-              <img 
-                src={avatarPreview} 
-                alt="Avatar" 
-                className="w-full h-full object-cover"
-              />
-              <label className="absolute top-0 left-1 bg-white p-2 rounded-full shadow-md cursor-pointer hover:bg-primary" title="Changer la photo">
-                <Camera size={16} className="text-gray-600" />
-                <input 
-                  type="file" 
-                  onChange={handleAvatarChange} 
-                  className="hidden" 
-                  accept="image/*"
-                />
-              </label>
+      {/* Avatar Section */}
+      <div className="relative flex-shrink-0">
+        <div className="relative w-32 h-32 rounded-full border-4 border-white shadow-lg -mt-16 bg-neutral">
+
+          {/* Loader ou Image */}
+          {avatarUploading ? (
+            <div className="absolute inset-0 flex items-center justify-center pointer-events-none rounded-full overflow-hidden">
+              <span className="loading loading-bars text-primary loading-md"></span>
             </div>
-          </div>
+          ) : (
+            <motion.img
+              key={avatarPreview} 
+              src={avatarPreview}
+              alt="Avatar"
+              className="w-full h-full object-cover rounded-full"
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 200, damping: 20 }}
+            />
+          )}
+
+          {/* Bouton Camera */}
+          <motion.label
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="absolute -top-2 left-1 bg-white p-2 rounded-full shadow-md cursor-pointer hover:bg-primary"
+            title="Changer la photo"
+          >
+            <Camera size={16} className="text-gray-600" />
+            <input 
+              type="file" 
+              onChange={handleAvatarChange} 
+              className="hidden" 
+              accept="image/*"
+            />
+          </motion.label>
+
+        </div>
+      </div>
+
 
           {/* Profile Form */}
           {isEditing ? (
