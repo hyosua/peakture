15b9ed56f6bea65a62d40f25e29f73a0973172e1
hyosua <colleterhyosua@gmail.com>
2025-04-29 14:54:34 +0200

    update vite url

--- server/lib/utils/generateToken.js
+++ server/lib/utils/generateToken.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken'
 import { v4 as uuidv4 } from 'uuid' // génère un sessionid unique
 
 export const generateTokenAndSetCookie = (res, userId=null) => {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 15);
 
     if(userId){
         const token = jwt.sign({ userId }, process.env.JWT_SECRET,{
@@ -10,7 +12,7 @@ export const generateTokenAndSetCookie = (res, userId=null) => {
 
         res.cookie("jwt", token,{
             domain: '.peakture.fr',
-            path: '/',
+            maxAge: 15*24*60*60*1000, //15 days in ms
             httpOnly: true, // prevent XSS attacks cross-site scripting attacks
             sameSite:"None", // CSRF attacks cross-site request forgery attacks
             secure: true, 
@@ -21,7 +23,7 @@ export const generateTokenAndSetCookie = (res, userId=null) => {
 
         res.cookie("sessionId", sessionId,{
             domain: '.peakture.fr',
-            path: '/',
+            maxAge: 24 * 60 * 60 * 1000, // 1 jour en millisecondes
             httpOnly: false, // prevent XSS attacks cross-site scripting attacks
             sameSite:"None", // CSRF attacks cross-site request forgery attacks
             secure: true, 
